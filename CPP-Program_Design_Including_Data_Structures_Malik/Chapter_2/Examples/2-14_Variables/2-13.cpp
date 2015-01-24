ff9224ccee39c0cd5d3168603f9f670b093e15df
odelik <joshuaiancarter@gmail.com>
2015-01-24 15:44:26 -0800

    2-13: Investigate declaring and using variables

---
+++ CPP-Program_Design_Including_Data_Structures_Malik/Chapter_2/Examples/2-14_Variables/2-13.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "2-13.h"
+
+using namespace std;
+
+int main()
+{
+	int i, j;
+	double sale;
+	char first;
+	string words;
+
+	i = 4;
+	cout << "i = " << i << endl;
+
+	j = 4 * 5 - 11;
+	cout << "j = " << j << endl;
+
+	sale = 0.02 * 1000;
+	cout << "sale = " << sale << endl;
+
+	first = 'D';
+	cout << "first = " << first << endl;
+
+	words = "It is a sunny day!";
+	cout << "words = " << words << endl;
+
+
+	return 0;
+}
