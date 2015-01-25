7ed54124897bfa60e65a35827256486af09c2272
odelik <joshuaiancarter@gmail.com>
2015-01-24 22:19:21 -0800

    2-21: Investigate output
    * Investigate different ways to display output

---
+++ CPP-Program_Design_Including_Data_Structures_Malik/Chapter_2/Examples/2-21_output/2-21.cpp
@@ -0,0 +1,25 @@
+#include "2-21.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	a = 65;
+	b = 78;
+
+	cout << 28 / 4 << endl;
+	cout << 3.0 / 2 << endl;
+	cout << "Hello there.\n";
+	cout << 7 << endl;
+	cout << 3 + 5;
+	cout << endl;
+	cout << 2 + 3 * 6 << endl;
+	cout << "a" << endl;
+	cout << a << endl;
+	cout << b << endl;
+
+	return 0;
+}
