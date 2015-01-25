d26c07c5c830537b29a1cc34f35c4c585cae101b
odelik <joshuaiancarter@gmail.com>
2015-01-24 22:02:07 -0800

    2-16: Explore cin statements
    * Use cin to get user input
    * Store user input into variables

---
+++ CPP-Program_Design_Including_Data_Structures_Malik/Chapter_2/Examples/2-16_cin/2-16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "2-16.h"
+
+using namespace std;
+
+int main()
+{
+	int feet;
+	int inches;
+
+	char hold;
+
+	cout << "Enter two integers separated by spaces: ";
+	cin >> feet >> inches;
+	cout << endl;
+
+	cout << "Feet = " << feet << endl;
+	cout << "Inches = " << inches << endl;
+
+	return 0;
+}
